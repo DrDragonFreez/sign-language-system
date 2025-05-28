4e5129dfcfaa8e3c10f0b2d226fab05cbf152ef6
To4kaGT <sulyeve@gmail.com>
2025-05-28 16:40:51 +0300

    GUI и Main

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from GUI import GestureApp
 import tkinter as tk
+import customtkinter as ctk
 
 if __name__ == "__main__":
-    root = tk.Tk()
+    ctk.set_appearance_mode("dark")
+    ctk.set_default_color_theme("blue")
+
+    root = ctk.CTk()
     app = GestureApp(root)
     root.mainloop()
